8e016cfd334242871affd4a5c2861d3549694c55
Matthew Nguyen <Matthew.Nguyen@mouser.com>
2023-10-17 14:19:49 -0500

    a

--- src/javascript.js
+++ src/javascript.js
@@ -1,5 +1,4 @@
 
-
 // api url
 const api_url =
     "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/MmmmmmTacos?api_key=RGAPI-2a2fe529-accb-4cd9-a70a-281e9eb39b2f";
@@ -27,22 +26,11 @@ function hideloader() {
 }
 // Function to define innerHTML for HTML table
 function show(data) {
-    let tab =
-        `<tr>
-          <th>Name</th>
-          <th>Office</th>
-          <th>Position</th>
-          <th>Salary</th>
-         </tr>`;
+    let tab = "  <tr>  <th>Name</th> <th>Office</th> <th>Position</th><th>Salary</th> </tr>";
 
     // Loop to access all rows 
     for (let r of data.list) {
-        tab += `<tr> 
-    <td>${r.name} </td>
-    <td>${r.office}</td>
-    <td>${r.position}</td> 
-    <td>${r.salary}</td>          
-</tr>`;
+        tab += "<tr>   <td>${r.name} </td> <td>${r.office}</td> <td>${r.position}</td>  <td>${r.salary}</td>   </tr>";
     }
     // Setting innerHTML as tab variable
     document.getElementById("employees").innerHTML = tab;
