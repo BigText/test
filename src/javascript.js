3182847c18dae0804c9b80ec58a7b9556f53fecf
Matthew Nguyen <Matthew.Nguyen@mouser.com>
2023-10-17 16:11:01 -0500

    a

--- src/javascript.js
+++ src/javascript.js
@@ -2,10 +2,9 @@
 // api url
 
 // Defining async function
-async function getapi() {
+async function getapi(summonerName) {
 
-    const url =
-        "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/MmmmmmTacos?api_key=RGAPI-2a2fe529-accb-4cd9-a70a-281e9eb39b2f";
+   const url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+summonerName+"?api_key=RGAPI-2a2fe529-accb-4cd9-a70a-281e9eb39b2f";
 
     // Storing response
     const response = await fetch(url);
