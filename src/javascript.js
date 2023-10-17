e0bb6a9bec941112c9a37b4b1f88b26ef03f42e4
Matthew Nguyen <Matthew.Nguyen@mouser.com>
2023-10-17 14:30:46 -0500

    a

--- src/javascript.js
+++ src/javascript.js
@@ -12,25 +12,19 @@ async function getapi(url) {
     // Storing data in form of JSON
     var data = await response.json();
     console.log(data);
-    if (response) {
-        hideloader();
-    }
+
     show(data);
 }
 // Calling that async function
 getapi(api_url);
 
-// Function to hide the loader
-function hideloader() {
-    document.getElementById('loading').style.display = 'none';
-}
 // Function to define innerHTML for HTML table
 function show(data) {
-    let tab = "  <tr>  <th>Name</th> <th>Office</th> <th>Position</th><th>Salary</th> </tr>";
+    let tab = "  <tr>  <th>id</th> <th>Name</th> <th>Level</th></tr>";
 
     // Loop to access all rows 
     for (let r of data.list) {
-        tab += "<tr>   <td>${r.name} </td> <td>${r.office}</td> <td>${r.position}</td>  <td>${r.salary}</td>   </tr>";
+        tab += "<tr>   <td>${r.id} </td> <td>${r.name}</td> <td>${r.level}</td>   </tr>";
     }
     // Setting innerHTML as tab variable
     document.getElementById("employees").innerHTML = tab;
