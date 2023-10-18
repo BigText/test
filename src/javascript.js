6675c19ea8fb27dbdc0b58ecf457a948539bc67f
Matthew Nguyen <Matthew.Nguyen@mouser.com>
2023-10-18 13:43:11 -0500

    a

--- src/javascript.js
+++ src/javascript.js
@@ -17,12 +17,24 @@ async function getapi(summonerName) {
 }
 
 // Function to define innerHTML for HTML table
+async function getMatchHistory(puuid)
+{
+    const url = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "?api_key=RGAPI-2a2fe529-accb-4cd9-a70a-281e9eb39b2f";
+    const response = await fetch(url);
+
+    var data = await response.json();
+    console.log(data);
+    showMH(data);
+}
 function show(data) {
-    let tab = "  <tr>  <th>Name</th> <th>Level</th></tr>";
-
-
-    tab += "<tr>  <td> " + data.name + "</td> <td> " + data.summonerLevel + "  </td>   </tr > ";
-    
+    getMatchHistory(data.puuid)
     // Setting innerHTML as tab variable
-    document.getElementById("employees").innerHTML += tab;
+    document.getElementById("employees").innerHTML += "<tr>  <td> " + data.name + "</td> <td> " + data.summonerLevel + "  </td>   </tr > "
+}
+function showMH(data)
+{
+    data.foreach(data)
+    {
+
+    }
 }
